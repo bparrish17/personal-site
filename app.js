949d0a1c55de44eaefeed303d7bf0fbdba7f7ad0
Brian Parrish <brian.parrish3739@gmail.com>
2020-09-12 14:34:34 -0400

    created file structure

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const nunjucks = require('nunjucks')
+const path = require('path')
+const app = express()
+const port = 3000
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+nunjucks.configure('views', {
+    autoescape: true,
+    express: app,
+    watch: true
+});
+
+app.get('/', (req, res) => {
+  res.render('index.html');
+});
+
+app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}`)
+})
