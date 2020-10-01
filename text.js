e3eeabff9638b865ffddc9efd04edb4b3fd91674
Brian Parrish <brian.parrish3739@gmail.com>
2020-09-30 21:41:58 -0400

    Added skills along with needed logos. Updated hero and background to be more simple and brigther

--- text.js
+++ text.js
@@ -26,22 +26,71 @@ const EMPLOYMENT_HISTORY = [
 
 const EDUCATION_HISTORY = [
   {
-  link: 'http://fullstackacademy.com',
-  image: 'img/fsa_logo.png',
-  companyName: 'Fullstack Academy',
-  jobTitle: 'software engineering immersive bootcamp',
-  description: GENERIC_DESCRIPTION
-},
-{
-  link: 'http://sc.edu',
-  image: 'img/uschc_logo.png',
-  companyName: 'University of South Carolina',
-  jobTitle: 'darla moore school of business',
-  description: GENERIC_DESCRIPTION
-}]
+    link: 'http://fullstackacademy.com',
+    image: 'img/fsa_logo.png',
+    companyName: 'Fullstack Academy',
+    jobTitle: 'software engineering immersive bootcamp',
+    description: GENERIC_DESCRIPTION
+  },
+  {
+    link: 'http://sc.edu',
+    image: 'img/uschc_logo.png',
+    companyName: 'University of South Carolina',
+    jobTitle: 'darla moore school of business',
+    description: GENERIC_DESCRIPTION
+  }
+]
+
+const PROFESSIONAL_SKILLS = [
+  {
+    name: 'Typescript',
+    image: 'icons/typescript_logo.png'
+  },
+  {
+    name: 'Angular',
+    image: 'icons/angular_logo.png'
+  },
+  {
+    name: 'NodeJS',
+    image: 'icons/node_logo.png'
+  },
+  {
+    name: 'HTML5',
+    image: 'icons/html5_logo.png'
+  },
+  {
+    name: 'CSS3',
+    image: 'icons/css_logo.png'
+  },
+  {
+    name: 'ReactJS',
+    image: 'icons/react_logo.png'
+  },
+]
+
+const PROFESSIONAL_TECH = [
+  {
+    name: 'AWS',
+    image: 'icons/aws_logo.png'
+  },
+  {
+    name: 'Jenkins',
+    image: 'icons/jenkins_logo.png'
+  },
+  {
+    name: 'RxJS',
+    image: 'icons/rxjs_logo.png'
+  },
+  {
+    name: 'SCSS',
+    image: 'icons/scss_logo.png'
+  },
+]
 
 module.exports = {
   GENERIC_DESCRIPTION,
   EMPLOYMENT_HISTORY,
-  EDUCATION_HISTORY
+  EDUCATION_HISTORY,
+  PROFESSIONAL_SKILLS,
+  PROFESSIONAL_TECH
 }
